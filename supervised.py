17a352c92798f7a9db460e1b44fdad37617418c9
Shreya Gupta <89708685+shreyagupta1995@users.noreply.github.com>
2021-12-15 19:29:43 -0500

    changes

--- supervised.py
+++ supervised.py
@@ -54,11 +54,10 @@ data_dir = "/scratch/sg6606/data"
 TRANSFORM_IMG = {
     "train": transforms.Compose(
         [
-            transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
+            transforms.Resize((256, 256)),
             transforms.RandomRotation(degrees=15),
-            transforms.ColorJitter(),
+            transforms.ColorJitter(contrast = (5)),
             transforms.RandomHorizontalFlip(),
-            transforms.CenterCrop(size=224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ]
